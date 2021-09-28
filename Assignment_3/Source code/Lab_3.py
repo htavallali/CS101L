d8b13eec8584fcdd8261c2f9bc37dab3ac2aab87
htavallali <90810564+htavallali@users.noreply.github.com>
2021-09-28 03:20:13 -0500

    Add files via upload

---
+++ Assignment_3/Source code/Lab_3.py
@@ -0,0 +1,45 @@
+print("Welcome to the Flarsheim Gusser!\n")
+
+while(True):
+    print("Please think of a number between and including 1 and 100.\n")
+    # to access 3 5 or 7
+    values="357"
+    # to store remainders
+    res=[]
+    for i in values:
+        while(True):
+            print("What is the remainder when your number is divide by ",i," ?",end="")
+            r=int(input())
+            if(r<0):
+                print("The value entered must be 0 or greater")
+                continue
+            elif(r>=int(i)):
+                print("The value entered must be less than ",i)
+                continue
+            else:
+                if(i!='7'):
+                    print()
+                res.append(r)
+                break
+    #Numbers 1 to 100 inclusive
+    for val in range(1,101):
+        if(val%3 == res[0] and val%5 == res[1] and val%7 == res[2]):
+            print("Your number was ",str(val))
+            print("How amazing is that?\n")
+            break
+
+    #to get choice from user
+    choice=''
+    while(True):
+        print("Do you want to play the game again?Y to continue,N to quit ==> ",end="")
+        choice=input()
+        if(choice=='n' or choice=='N' or choice=='y' or choice=='y'):
+            break
+        else:
+            continue
+        
+    if(choice=='n' or choice=='N'):
+        break
+    elif(choice=='y' or choice=='Y'):
+        print()
+        continue
